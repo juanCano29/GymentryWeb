05db3a12d9ff06a62ce2275ce155039b9697d5a0
ZClauen <axsr_97@hotmail.com>
2020-03-27 13:35:23 -0600

    agregando view de crear productos, modificar descuentos y un modificar un controller

--- app/Http/Controllers/prueba.php
+++ app/Http/Controllers/prueba.php
@@ -18,4 +18,25 @@ class prueba extends Controller
     	$user = DB::connection('mysql')->select('select * from prueba');
     	return $user;
     }
+
+     public function ver_nombres_productos(){
+    
+        $productos_array = DB::table('productos')->select('nombre')->get();
+        $nombre = json_encode($productos_array[0]->nombre);
+
+        return view('CrearProducto',compact('nombre'));
+
+    //  $consulta = DB::table('productos')
+    //         ->select('*')     
+    //         ->get();
+    //  return $consulta;
+
+    //  $consulta = DB::table('usuarios')
+    //          ->select('*')
+    //         ->join('productos', 'productos.producto_id', '=', 'usuarios.usu_id')
+    //         ->orWhere('cli_rfc', 'LIKE', '%'.$request->datos["rfc"].'%')
+    //         ->get();
+    //  return $consulta;
+
+    }
 }
